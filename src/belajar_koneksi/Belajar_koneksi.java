76464809e92f487a219f0014b98d1a6058c30be9
jakajunaedi <jaka.junaedi03@gmail.com>
2020-08-05 17:17:35 +0700

    mencoba koneksi ke postgresql

--- src/belajar_koneksi/Belajar_koneksi.java
+++ src/belajar_koneksi/Belajar_koneksi.java
@@ -1,7 +1,26 @@
 package belajar_koneksi;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
 public class Belajar_koneksi {
     public static void main(String[] args) {
 
+        Connection connection=null;
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+            connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password051455");
+
+            if (connection!=null) {
+                System.out.println("konek database ok");
+            }else{
+                System.out.println("konek tidak");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
